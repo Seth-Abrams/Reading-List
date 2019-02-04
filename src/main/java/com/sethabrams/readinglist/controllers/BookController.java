5c6761ff37de9d69e37e2f74d2fa17685afabd30
Seth-Abrams <43708866+Seth-Abrams@users.noreply.github.com>
2019-02-04 14:46:52 -0500

    bookcontroller

---
+++ src/main/java/com/sethabrams/readinglist/controllers/BookController.java
@@ -0,0 +1,47 @@
+package com.sethabrams.readinglist.controllers;
+
+import com.sethabrams.readinglist.domain.Book;
+import com.sethabrams.readinglist.services.BookServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BookController {
+
+    private final BookServiceLayer bookServiceLayer;
+
+    @Autowired
+    public BookController(BookServiceLayer bookServiceLayer) {
+        this.bookServiceLayer = bookServiceLayer;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return bookServiceLayer.getAllBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<?> getBook(@PathVariable Long id){
+        return bookServiceLayer.getBook(id);
+    }
+
+    @PostMapping("/books/")
+    public ResponseEntity<Book> addBook(@RequestBody Book book) throws URISyntaxException {
+        return bookServiceLayer.addBook(book);
+    }
+
+    @PutMapping("/books")
+    public ResponseEntity<Book> updateBook(@RequestBody Book book){
+        return bookServiceLayer.updateBook(book);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id){
+        return bookServiceLayer.deleteBook(id);
+    }
+}
