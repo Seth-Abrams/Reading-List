dd4b5ae78126a816dfbe8662776c655afa3a2923
Seth-Abrams <43708866+Seth-Abrams@users.noreply.github.com>
2019-02-04 14:06:24 -0500

    initial commit/book entity

---
+++ src/main/java/com/sethabrams/readinglist/ReadingList.java
@@ -0,0 +1,14 @@
+package com.sethabrams.readinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingList {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingList.class, args);
+	}
+
+}
+
